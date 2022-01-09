0a266eca62ca608731808901880c0de25706ca21
kytan1314 <kytan1314@gmail.com>
2022-01-09 04:26:56 +0000

    done part 4

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<!-- Sign Out -->
+<script>
+    import supabase from '$lib/db';
+
+    async function logout() {
+   	 const { error } = await supabase.auth.signOut();
+
+   	 if (error) alert(error.message); // alert if error
+    }
+
+</script>
+<section class="container px-4 py-3 text-center">
+    <button class="btn btn-secondary" on:click={logout}>Logout</button>
+</section>
+
