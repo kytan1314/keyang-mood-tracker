28fc809398b8f8b75237c530fbb0696d27c94e51
kytan1314 <kytan1314@gmail.com>
2022-01-23 04:11:30 +0000

    done

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-   	 // hydrate the <div id="svelte"> element in src/app.html
-   	 target: '#svelte',
-   	 adapter: vercel()
-    }
+	kit: {
+		adapter: adapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
 };
 
-export default config;
